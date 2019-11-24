03708f2a255b6e8159b0faffa15c78cc300f7ce5
Owen Jow <owenjow@berkeley.edu>
2019-11-23 20:34:43 -0800

    key events

--- renderloop.py
+++ renderloop.py
@@ -6,28 +6,79 @@ import cv2
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import loadPrcFileData
 from image_styler import ImageStyler
+from pynput import keyboard
 
 loadPrcFileData('', 'window-type offscreen')
 loadPrcFileData('', 'sync-video 0')
 
+KEY_A = keyboard.KeyCode.from_char('a')
+KEY_S = keyboard.KeyCode.from_char('s')
 
-def show_rgb_image(image, window_name, delay=1):
-    # image should be in BGR format
-    cv2.imshow(window_name, image)
-    key = cv2.waitKey(delay)
-    key &= 255
-    if key == 27 or key == ord('q'):
-        print('pressed ESC or q, exiting')
-        exit_request = True
-    else:
-        exit_request = False
-    return exit_request
+
+class OutputWindow:
+    def __init__(self, window_name):
+        self.window_name = window_name
+
+        # Store which keys are currently pressed.
+        self.key_state = {
+            'left':      False,
+            'right':     False,
+            'forward':   False,
+            'backward':  False,
+            'cam-left':  False,
+            'cam-right': False
+        }
+
+        self.listener = keyboard.Listener(
+            on_press=self.on_key_press,
+            on_release=self.on_key_release)
+        self.listener.start()
+
+    def show_rgb_image(self, image, delay=1):
+        # image should be in BGR format
+        cv2.imshow(self.window_name, image)
+        key = cv2.waitKey(delay)
+        key &= 255
+        if key == 27 or key == ord('q'):
+            print('pressed ESC or q, exiting')
+            return False
+        return True  # the show goes on
+
+    def on_key_press(self, key):
+        if key == keyboard.Key.left:
+            self.key_state['left'] = True
+        elif key == keyboard.Key.right:
+            self.key_state['right'] = True
+        elif key == keyboard.Key.up:
+            self.key_state['up'] = True
+        elif key == keyboard.Key.down:
+            self.key_state['down'] = True
+        elif key == KEY_A:
+            self.key_state['cam-left'] = True
+        elif key == KEY_S:
+            self.key_state['cam-right'] = True
+
+    def on_key_release(self, key):
+        if key == keyboard.Key.left:
+            self.key_state['left'] = False
+        elif key == keyboard.Key.right:
+            self.key_state['right'] = False
+        elif key == keyboard.Key.up:
+            self.key_state['up'] = False
+        elif key == keyboard.Key.down:
+            self.key_state['down'] = False
+        elif key == KEY_A:
+            self.key_state['cam-left'] = False
+        elif key == KEY_S:
+            self.key_state['cam-right'] = False
 
 
 class BeautyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
+        # Disable the camera trackball controls.
+        self.disableMouse()
         # Load the environment model.
         self.scene = self.loader.loadModel('models/environment')
         # Reparent the model to render.
@@ -63,6 +114,7 @@ if __name__ == '__main__':
 
     app = BeautyApp()
     window_name = 'IN PURSUIT OF BEAUTY'
+    output_window = OutputWindow(window_name)
 
     frames = 1800
     radius = 20
@@ -83,8 +135,8 @@ if __name__ == '__main__':
         image = np.transpose(image.numpy().squeeze(), (1, 2, 0))
         image = image[:, :, ::-1]  # RGB -> BGR
 
-        exit_request = show_rgb_image(image, window_name)
-        if exit_request:
+        # show
+        if not output_window.show_rgb_image(image):
             break
 
     end_time = time.time()
